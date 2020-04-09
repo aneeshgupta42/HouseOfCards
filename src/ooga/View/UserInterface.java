8fadbd77237d74fe60a35709302e8ad2f7ccf303
Aneesh Gupta <ag468@duke.edu>
2020-04-09 14:37:24 +0530

    beginning work on UI

---
+++ src/ooga/View/UserInterface.java
@@ -0,0 +1,67 @@
+package ooga.View;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application implements Viewable {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+
+    @Override
+    public void moveCard(int id, int x, int y) {
+
+    }
+
+    @Override
+    public void updateScore(int score) {
+
+    }
+
+    @Override
+    public void displayCard(Image cardView) {
+
+    }
+
+    @Override
+    public void makeDeck(Deck deck) {
+
+    }
+
+    @Override
+    public void downloadGame(Game game) {
+
+    }
+
+    @Override
+    public void gameMessage(String message) {
+
+    }
+
+    @Override
+    public void getNewCard(Deck deck) {
+
+    }
+
+    @Override
+    public void shuffleDeck(int deckId) {
+
+    }
+
+    @Override
+    public void generateGame(int gameChoice) {
+
+    }
+
+    @Override
+    public void endGame() {
+
+    }
+
+    @Override
+    public Deck getDeck(int x, int y) {
+        return null;
+    }
+}
