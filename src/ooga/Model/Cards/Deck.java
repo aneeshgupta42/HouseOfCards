fe581cb3db22c312ceb75ea7d48cda12e16673eb
Tharun Raj <tm272@duke.edu>
2020-04-12 21:18:28 +0800

    Improved efficiency of card creation

--- src/ooga/Model/Cards/Deck.java
+++ src/ooga/Model/Cards/Deck.java
@@ -10,4 +10,16 @@ public interface Deck {
     int getDeckSize();
     List<Playable> getCards();
 
+    /**
+     * Removes numOfCards Playable cards from the deck and returns it in a list.
+     * @param numOfCards
+     * @return
+     */
+    List<Playable> popCards(int numOfCards);
+
+    /**
+     * return a deep copy of a cardDeck object. All Playable objects in the deck will be copied as well.
+     * @return
+     */
+    CardDeck returnADeepCopy();
 }
