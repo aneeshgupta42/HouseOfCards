2c80927968b35968a6c7fb6e6b2b3e15f3b1cbe8
Himanshu Jain <hkj4@dj4.edu>
2020-04-14 15:15:30 +0530

    Set up the buttons and other features

--- src/ooga/View/GameScreens/GameScreen.java
+++ src/ooga/View/GameScreens/GameScreen.java
@@ -1,8 +1,26 @@
 package ooga.View.GameScreens;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import ooga.View.ButtonFactory;
+import ooga.View.UserInput;
 
 public abstract class GameScreen {
+    private ButtonFactory exitButton;
+    private ButtonFactory restartButton;
+    private double xpos=20;
+    private double ypos=600;
+    private double endXpos=1030;
+    public void setUpButtons(Group gameScene){
+        exitButton = new ButtonFactory("Exit", xpos,ypos );
+        restartButton = new ButtonFactory("Restart",endXpos,ypos);
+        gameScene.getChildren().addAll(exitButton,restartButton);
+    }
+//    public void userScreen(String gameName){
+//        UserInput c= new UserInput(gameName, this);
+//        myStage.setScene(c.getUserScene());
+//        myStage.show();
+//    }
 
 }
