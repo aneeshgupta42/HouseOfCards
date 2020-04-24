c02cb7ef51f7468adf5aab095b89925f018ae730
Tharun Raj <tm272@duke.edu>
2020-04-24 22:00:15 +0800

    makePiles done for Memory

--- src/ooga/Model/Games/MemoryDriver.java
+++ src/ooga/Model/Games/MemoryDriver.java
@@ -1,16 +1,37 @@
 package ooga.Model.Games;
 
+import ooga.Controller.DeckType;
 import ooga.Controller.GameController;
+import ooga.Model.Cards.CardDeck;
+import ooga.Model.Cards.Deck;
+import ooga.Model.Cards.Playable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MemoryDriver extends GameDriver {
-    public MemoryDriver(GameController controller) {
+    public MemoryDriver(GameController controller, List<String>playerNames) {
         super(controller);
+        makePlayers(playerNames);
+        makePiles();
     }
 
     @Override
     protected void makePiles() {
+        CardDeck completeSet = new CardDeck(DeckType.POKER);
+        CardDeck deckWithSpecifiedSuits = new CardDeck(DeckType.POKER, new ArrayList<>());
+        for (Playable card : completeSet.getCards()){
+            if (card.getValue().equals("S") || card.getValue().equals("D")){
+                deckWithSpecifiedSuits.addCard(card);
+            }
+        }
+        deckWithSpecifiedSuits.addCard(deckWithSpecifiedSuits.returnADeepCopy().getCards());
+        deckWithSpecifiedSuits.shuffleDeck();
+        //System.out.println(deckWithSpecifiedSuits);
+        for(int i = 0; i < 4; i++){
+            piles.put(i, new CardDeck(DeckType.POKER, deckWithSpecifiedSuits.popCards(13)));
+        }
+        //System.out.println(deckWithSpecifiedSuits.getDeckSize());
 
     }
 
@@ -47,5 +68,13 @@ public class MemoryDriver extends GameDriver {
     @Override
     public void startGame() {
 
+    }
+
+    public static void main(String[] args) {
+        MemoryDriver test = new MemoryDriver(new GameController(), new ArrayList<>());
+        for (Integer i : test.piles.keySet()){
+            System.out.println("Pile:"+ i);
+            System.out.println(test.piles.get(i));
+        }
     }
 }
